bd6f5e1dba076b35c3c785a56fce52345eedcc06
Mateo Garcia <garciacarrascomat@gmail.com>
2026-02-27 14:06:30 +0100

    feat: atributos

--- jjba-rpg-oop/src/estado/Estado.java
+++ jjba-rpg-oop/src/estado/Estado.java
@@ -1,5 +1,10 @@
 package estado;
 
 public abstract class Estado {
-	
+String nombre;
+int turnosRestantes;
+int potenciaPorTurno;
+boolean apilable;
+//origen
+
 }
--- jjba-rpg-oop/src/movimiento/Movimiento.java
+++ jjba-rpg-oop/src/movimiento/Movimiento.java
@@ -1,5 +1,13 @@
 package movimiento;
 
 public abstract class Movimiento {
+String nombre;
+int potencia;
+double precision;
+int pp;
+int costeEnergia;
+String efectoSecundario;
+int prioridad;
+String efecto;
 
 }
